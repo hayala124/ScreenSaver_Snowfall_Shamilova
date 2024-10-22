80cb30b7939950f54364976d4182ec195b4b9312
hayala124 <elnur7803@gmail.com>
2024-10-22 21:32:50 +0300

    start

---
+++ ScreenSaver_Snowfall_Shamilova/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScreenSaver_Snowfall_Shamilova
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Screen screen = new Screen())
+            {
+                screen.Run();
+            }
+        }
+    }
+}
---
+++ ScreenSaver_Snowfall_Shamilova/Utilities/Global.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace ScreenSaver_Snowfall_Shamilova
+{
+    static class Global
+    {
+        public static Screen screen;
+        public static Random random = new Random();
+        public static string levelName;
+
+        public static void Initialize(Screen inputGame)
+        {
+            screen = inputGame;
+        }
+    }
+}
